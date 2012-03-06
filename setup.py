ddc274a29499d872570e4d48a16a6dee27cc3c86
Ralph Bean <rbean@redhat.com>
2012-03-06 10:18:56 -0500

    Changed email address.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='toscawidgets2 wrapper for jquery gritter plugin.',
     long_description=long_description,
     author='Ralph Bean',
-    author_email='ralph.bean@gmail.com',
+    author_email='rbean@redhat.com',
     license='MIT',
     url='http://github.com/toscawidgets/tw2.jqplugins.gritter',
     install_requires=[
